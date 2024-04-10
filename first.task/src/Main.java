76306df169ccf9a85ad67660d3b7cd59aab9fcde
sladeshka <130031mail@mail.ru>
2024-04-10 19:27:52 +0300

    first lab

--- first.task/src/Main.java
+++ first.task/src/Main.java
@@ -2,14 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        testJavaTypes();
-
-        Scanner scanner = new Scanner(System.in);
-        String s1 = scanner.nextLine();
-        int h = scanner.nextInt();
-
-        System.out.println(s1);
-        System.out.println(h);
+//        testJavaTypes();
+        Lab1();
     }
 
     public static void testJavaTypes() {
@@ -89,9 +83,17 @@ public class Main {
         switcher(a);
         String result2 = switcher(a, true);
         System.out.println(result2);
+
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        int h = scanner.nextInt();
+
+        System.out.println(s1);
+        System.out.println(h);
     }
 
     public static void switcher (int number) {
+        int x;
         switch (number) {
             case 10:
                 x = number;
@@ -108,5 +110,42 @@ public class Main {
 
     public static String switcher (int number, boolean isString) {
         return "String";
+    }
+
+    public static void Lab1 () {
+        // first task
+        System.out.println("I");
+        System.out.println("know");
+        System.out.println("Java");
+        System.out.println("well");
+        // second task
+        double x = ( 46 + 10 ) * ( 10 / 3 );
+        double y = ( 29 ) * ( 4 ) * (- 15 );
+        // third task
+        int number = 10500;
+        double result = (number / 10) / 10;
+        // fourth task
+        result = 3.6 * 4.1 * 5.9;
+        // fifth task
+        // 42
+        // 100
+        // 125
+        Scanner scanner = new Scanner(System.in);
+        int s1 = scanner.nextInt();
+        System.out.println(s1);
+        scanner = new Scanner(System.in);
+        s1 = scanner.nextInt();
+        System.out.println(s1);
+        scanner = new Scanner(System.in);
+        s1 = scanner.nextInt();
+        System.out.println(s1);
+        scanner = new Scanner(System.in);
+        s1 = scanner.nextInt();
+        String message = s1 % 2 > 0? "Odd" : "Even";
+        if(s1 > 100) {
+            message = "Out of range";
+        }
+        System.out.println(message);
+        // ! Использовать класс Scanner: int b = scanner.nextInt();
     }
 }
