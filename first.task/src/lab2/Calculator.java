d1e09622160303a9eaa830f9dfe5a89b82cc2bf2
sladeshka <130031mail@mail.ru>
2024-04-11 18:01:08 +0300

    add lab 2 and update lesson 2

---
+++ first.task/src/lab2/Calculator.java
@@ -0,0 +1,40 @@
+package lab2;
+
+public class Calculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static long add(long a, long b) {
+        return a + b;
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+    public static long divide(long a, long b) {
+        return a / b;
+    }
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static long multiply(long a, long b) {
+        return a * b;
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+    public static long subtract(long a, long b) {
+        return a - b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
