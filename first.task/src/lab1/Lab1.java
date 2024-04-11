98c85d6cd92d9cf5379ef405a216fe61364a07b7
sladeshka <130031mail@mail.ru>
2024-04-11 18:19:44 +0300

    fixed lab 1 and update main

--- first.task/src/lab1/Lab1.java
+++ first.task/src/lab1/Lab1.java
@@ -10,7 +10,7 @@ public class Lab1 {
         System.out.println("Java");
         System.out.println("well");
         // second task
-        double x = ( 46 + 10 ) * ( 10.0 / 3.0 );
+        double x = ( 46.0 + 10.0 ) * ( 10.0 / 3.0 );
         double y = ( 29 ) * ( 4 ) * (- 15 );
         // third task
         int number = 10500;
@@ -20,17 +20,15 @@ public class Lab1 {
         System.out.println(result);
         // fourth task
         result = 3.6 * 4.1 * 5.9;
+        System.out.println(result);
         // fifth task // 42 // 100 // 125
         Scanner scanner = new Scanner(System.in);
         int s1 = scanner.nextInt();
         System.out.println(s1);
-        scanner = new Scanner(System.in);
         s1 = scanner.nextInt();
         System.out.println(s1);
-        scanner = new Scanner(System.in);
         s1 = scanner.nextInt();
         System.out.println(s1);
-        scanner = new Scanner(System.in);
         s1 = scanner.nextInt();
         String message = s1 % 2 > 0? "Odd" : "Even";
         if(s1 > 100) {
