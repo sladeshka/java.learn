2bb118cdf1fdba5d3947f55dcc0ed634b7783ea8
sladeshka <130031mail@mail.ru>
2024-04-15 20:45:09 +0300

    add third lesson

--- first.task/src/Main.java
+++ first.task/src/Main.java
@@ -1,5 +1,7 @@
 import Lesson2.Car;
 import Lesson2.CarService;
+import lesson3.StaticExample;
+import lesson4.Array;
 
 import static lab1.Lab1.Lab1;
 import static lesson1.Types.testJavaTypes;
@@ -8,5 +10,7 @@ public class Main {
     public static void main(String[] args) {
 //        testJavaTypes();
         Lab1();
+        new StaticExample();
+        Array.main(args);
     }
 }
