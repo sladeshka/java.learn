f2f2045fc2eae864d099d650da4939a6cca14e23
sladeshka <130031mail@mail.ru>
2024-04-26 18:23:06 +0300

    add tasks without last

--- first.task/src/lab4/lab4.java
+++ first.task/src/lab4/lab4.java
@@ -1,15 +1,50 @@
 package lab4;
 
+import Lesson2.Car;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class lab4 {
-    public static void lab4 () {
-        // first task
-        for (int i = 1; i <= 99; i += 2) {
+    public static void main(String[] args) {
+        lab4();
+    }
+
+    public static void lab4() {
+        // first task part one
+        printOddNumber();
+        // second task part one
+        printNumberCanBeDividedWithoutRemainder();
+        // third task part one
+        calculateSumXAndYAndCompareWithZ();
+        // fourth task part one
+        compareXYZ();
+        // fifth task part one
+        int[] task5Part1 = {3, -3, 7, 4, 5, 4, 3};
+        check3BeginningAndEnd(task5Part1);
+        // sixth task part one
+        check3or1(task5Part1);
+        // first task part two
+        int[] task1Part2Test1 = {1, 2, 3, 4, 5};
+        int[] task1Part2Test2 = {1, 2, 3, 6, 5};
+        checkArraySorting(task1Part2Test1);
+        checkArraySorting(task1Part2Test2);
+        // second task part two
+        printArrayByLength();
+        // third task part two
+        swapFirstAndLast(task1Part2Test2);
+        // fourth task part two
+        int[] task4Part2 = {1, 2, 3, 1, 2, 4};
+        findFirstUniqueNumber(task4Part2);
+    }
+
+    public static void printOddNumber() {
+        for (int i = 1; i <= 100; i += 2) {
             System.out.println(i);
         }
-        // second task
+    }
+
+    public static void printNumberCanBeDividedWithoutRemainder() {
         for (int i = 1; i <= 100; i += 1) {
             if (i % 3 == 0) {
                 System.out.println(i);
@@ -21,26 +56,35 @@ public class lab4 {
                 System.out.println(i);
             }
         }
-        // third task
+    }
+
+    public static void calculateSumXAndYAndCompareWithZ() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first number");
-        Integer x = scanner.nextInt();
+        int x = scanner.nextInt();
         System.out.println("Enter second number");
-        Integer y = scanner.nextInt();
+        int y = scanner.nextInt();
         System.out.println("Enter third number");
-        Integer z = scanner.nextInt();
+        int z = scanner.nextInt();
         System.out.println("Result: " + (x + y == z));
-        // fourth task
+    }
+
+    public static void compareXYZ() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first number");
-        x = scanner.nextInt();
+        int x = scanner.nextInt();
         System.out.println("Enter second number");
-        y = scanner.nextInt();
+        int y = scanner.nextInt();
         System.out.println("Enter third number");
-        z = scanner.nextInt();
+        int z = scanner.nextInt();
         System.out.println("Result: " + (z > y && y > x));
-        // fifth task
-        int[] array = {3, -3, 7, 4, 5, 4, 3};
+    }
+
+    public static void check3BeginningAndEnd(int[] array) {
         System.out.println("There " + (array[0] == 3 || array[array.length - 1] == 3 ? "are" : "is no") + " 3 in the array " + Arrays.toString(array));
+    }
+
+    public static void check3or1(int[] array) {
         boolean arrayHasValue = false;
         for (int number : array) {
             if (number == 1 || number == 3) {
@@ -48,7 +92,64 @@ public class lab4 {
                 break;
             }
         }
-        // sixth task
         System.out.println("There " + (arrayHasValue ? "are" : "are no") + " 3 or 1 in the array " + Arrays.toString(array));
+    }
+
+    public static void checkArraySorting(int[] array) {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+        if (isSorted) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Please, try again");
+        }
+    }
+
+    public static void printArrayByLength() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array length: ");
+        int length = scanner.nextInt();
+
+        int[] array = new int[length];
+
+        System.out.println("Enter " + length + " numbers:");
+
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Array of " + length + " elements:");
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void swapFirstAndLast(int[] array) {
+        if (array.length >= 2) {
+            int temp = array[0];
+            array[0] = array[array.length - 1];
+            array[array.length - 1] = temp;
+        }
+    }
+
+    public static void findFirstUniqueNumber(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < array.length; j++) {
+                if (i != j && array[i] == array[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                // found
+            }
+        }
+        // not found
     }
 }
